f6b7cec5c081ff0fc9883d177b222fb7271ba9ab
Luiz Gustavo Setten <consultor.119@7comm.com.br>
2021-09-24 11:12:52 -0300

    feat: validate permissions

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,9 @@
 import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 import { AuthContext } from "../contexts/AuthContext"
 import { AuthTokenError } from "../errors/AuthTokenError"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
@@ -15,7 +17,13 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email}</h1>
+    <>
+      <h1>Dashboard: {user?.email}</h1>
+
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
+    </>
   )
 }
 
